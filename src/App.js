4c1beb90975e04ef1d45766a2c457605574c2959
prasad98765 <pnchaudhari1996@gmail.com>
2020-03-14 10:50:55 +0530

    added : display Book ui

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Card from '../src/components/books.component.js'
+import {BookComponent} from '../src/components/books.component'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <div className="cardList">
+         {/* <BookComponent></BookComponent> */}
        <Card ></Card>
        <Card></Card>
        <Card></Card>
--- src/components/books.component.js
+++ src/components/books.component.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { Component } from "react";
+// import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
@@ -7,103 +7,100 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import image from "/home/admin1/bookdisplay/src/book.jpeg";
-// import { findByLabelText } from "@testing-library/react";
-// import { red } from "@material-ui/core/colors";
-import Tooltip from '@material-ui/core/Tooltip';
+import image from "/home/admin1/Desktop/Book Store Front End/Dispay Book UI/displaybooks_ui_dummy/src/book.jpeg";
+import Tooltip from "@material-ui/core/Tooltip";
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 180,
-    height: 290,
-    marginLeft: 90,
-    marginTop: 50
-  },
-  media: {
-    marginTop: 5,
-    height: 140,
-    width: 100,
-    marginLeft: 40
+class App extends Component {
+  render() {
+    return (
+      <Card
+        style={{ maxWidth: 180, height: 290, marginLeft: 90, marginTop: 50 }}
+      >
+        <CardActionArea>
+          <Tooltip
+            title="By default disabled elements like <button> do not trigger user interactions so a Tooltip will not activate on normal events like hover. To accommodate disabled elements, add a simple wrapper element, such as a span.
+⚠️      In order to work with Safari, you need at least one display block or flex item below the tooltip wrapper."
+            arrow
+          >
+            <CardMedia
+              style={{ marginTop: 5, height: 140, width: 100, marginLeft: 40 }}
+              image={image}
+            />
+          </Tooltip>
+          <CardContent>
+            <Typography
+              style={{
+                fontFamily: "Times New Roman",
+                color: "black",
+                marginTop: -10,
+                fontSize: 13,
+                marginLeft: 10
+              }}
+            >
+              Beauty and the Beast
+            </Typography>
+          </CardContent>
+          <CardContent>
+            <Typography
+              style={{
+                fontFamily: "Times New Roman",
+                color: "grey",
+                marginTop: -30,
+                fontSize: 10,
+                marginLeft: 10
+              }}
+            >
+              Gabrielle
+            </Typography>
+          </CardContent>
+          <CardContent>
+            <Typography
+              style={{
+                fontFamily: "Times New Roman",
+                color: "black",
+                fontSize: 10,
+                marginLeft: 10,
+                marginTop: -45
+              }}
+            >
+              RS 150
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions className="bookdiv">
+          <div className="button1">
+            <Button
+              style={{
+                border: "none",
+                color: "white",
+                textDecoration: "none",
+                display: "inlineBlock",
+                transitionDuration: 0.4,
+                width: 80,
+                height: 30,
+                fontSize: "0.23em"
+              }}
+            >
+              Add to Bag
+            </Button>
+          </div>
+          <div className="button2">
+            <Button
+              style={{
+                width: 70,
+                height: 30,
+                fontFamily: "Times New Roman",
+                color: "black",
+                fontSize: "0.27em"
+              }}
+            >
+              Wishlist
+            </Button>
+          </div>
+        </CardActions>
+      </Card>
+    );
   }
-});
-
-export default function MediaCard() {
-  const classes = useStyles();
-
-  return (
-    <Card className={classes.root}>
-      <CardActionArea>
-      <Tooltip title="By default disabled elements like <button> do not trigger user interactions so a Tooltip will not activate on normal events like hover. To accommodate disabled elements, add a simple wrapper element, such as a span.
-⚠️ In order to work with Safari, you need at least one display block or flex item below the tooltip wrapper." arrow>
-        <CardMedia
-          className={classes.media}
-          image={image}
-        />
-      
-        </Tooltip>
-        <CardContent>
-          <Typography
-            style={{
-              fontFamily: "Times New Roman",
-              color: "black",
-              marginTop: -10,
-              fontSize: 13,
-              marginLeft: 10
-            }}
-          >
-            Beauty and the Beast
-          </Typography>
-        </CardContent>
-        <CardContent>
-          <Typography
-            style={{
-              fontFamily: "Times New Roman",
-              color: "grey",
-              marginTop: -30,
-              fontSize: 10,
-              marginLeft: 10
-            }}
-          >
-            Gabrielle
-          </Typography>
-        </CardContent>
-        <CardContent>
-          <Typography
-            style={{
-              fontFamily: "Times New Roman",
-              color: "black",
-              fontSize: 10,
-              marginLeft: 10,
-              marginTop: -45
-            }}
-          >
-            RS 150
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions className="bookdiv">
-        <div className="button1">
-          <Button
-            style={{
-              border: "none",
-              color: "white",
-              textDecoration: "none",
-              display: "inlineBlock",
-              transitionDuration: 0.4,
-              width: 80,
-              height: 30,
-              fontSize: "0.23em"
-            }}
-          >
-            Add to Bag
-          </Button>
-        </div>
-        <div className="button2">
-          <Button style={{width:70,height:30, fontFamily: "Times New Roman", color: "black",   fontSize: "0.27em"}}>
-            Wishlist
-          </Button>
-        </div>
-      </CardActions>
-    </Card>
-  );
 }
+
+export default App;
